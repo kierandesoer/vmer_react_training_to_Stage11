fcb30e23ad0e7e17d9de0d21c8e29eff90fa16f6
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-05-23 12:45:18 +0100

    Initial commit of Exercise 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
-
+import React from 'react'; 
+import ClickButton from './ClickButton';
 
 export const App = () => {
+
   return (
-    <>
-      {/* stuff goes below... */}
-    </>
+    <div style={{ width: '100%', backgroundColor: 'red', color: 'white', textAlign: 'center', padding: '20px'}}>
+      <div>Hello World!</div>
+      <div style={{ marginTop: '20px' }}>
+        <ClickButton />
+      </div>
+    </div>
   );
 }

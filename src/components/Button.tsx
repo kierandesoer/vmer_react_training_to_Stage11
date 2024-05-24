f271544868b24444a9d1336cd165c2fbfa31b395
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-05-24 11:40:13 +0100

    Added formatting to button text in stage 6

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,12 +6,12 @@ export const Button = ({ buttonText, alertText, totalCount, incrementTotalCount 
     const onClick = () => {
         setCounter(counter + 1 );
         incrementTotalCount();
-        // alert("You've clicked me ${counter} times!\n You've clicked all the buttons a total of ${totalCount} times!");
+        alert(`You've clicked me ${counter} times!\nYou've clicked all the buttons a total of ${totalCount} times!`);
     }
 
     return (
         <div style={{ marginTop: '20px' }}>
-            <button onClick={onClick}>{buttonText} You've clicke me {counter}</button>
+            <button onClick={onClick}>{buttonText}<br />You've clicked me {counter}</button>
         </div>
     )
 }

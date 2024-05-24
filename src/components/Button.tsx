3ca29a76157c4eb77859ecb651a0dac92d2235a4
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-05-24 12:49:04 +0100

    Added functionality to display odd or even - Stage-7

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export const Button = ({ buttonText, alertText, totalCount, incrementTotalCount }) => {
     const [counter, setCounter] = useState(0);
+    const [isEven, setIsEven] = useState(true);
+
+    useEffect(() => {
+        setIsEven (counter % 2 === 0);
+    }, [counter]);
+
 
     const onClick = () => {
         setCounter(counter + 1 );
         incrementTotalCount();
-        alert(`You've clicked me ${counter} times!\nYou've clicked all the buttons a total of ${totalCount} times!`);
+        alert(`You've clicked me ${counter + 1} times!\nThis is an ${isEven ? 'even' : 'odd'} amount.\nYou've clicked all the buttons a total of ${totalCount + 1} times!`);
     }
 
     return (

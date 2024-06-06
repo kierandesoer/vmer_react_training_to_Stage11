da66aeebcfac7b76a4379c3d5f74165ec165ae4c
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-06-06 11:56:37 +0100

    Initial commit for Stage 10

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export const Button = ({ buttonText, alertText, totalCount, incrementTotalCount }) => {
-    const [counter, setCounter] = useState(0);
-    const [isEven, setIsEven] = useState(true);
+  const [count, setCount] = useState(0);
 
-    // Uncomment the following 3 lines and select the alternative alert in the onClick function 
-    // if you want the alert to display odd/even, counter and total count
-    // useEffect(() => {
-    //     setIsEven (counter % 2 === 0);
-    // }, [counter]);
-
-
-    const onClick = () => {
-        setCounter(counter + 1 );
-        incrementTotalCount();
-        alert(alertText);
-        // alert(`You've clicked me ${counter + 1} times!\nThis is an ${isEven ? 'even' : 'odd'} amount.\nYou've clicked all the buttons a total of ${totalCount + 1} times!`);
-    }
-
-    return (
-            <button style={{ marginRight: '20px' }} onClick={onClick}>{buttonText}<br />You've clicked me {counter}</button>
-    )
+  const onClick = () => {
+    alert(alertText);
+    setCount(count + 1);
+    incrementTotalCount();
+  }
+  return <button onClick={onClick}>{buttonText} Clicked {count} times! Total click count for all buttons is: {totalCount}!</button>
 }

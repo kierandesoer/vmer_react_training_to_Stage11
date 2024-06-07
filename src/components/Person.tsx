9b15433b2a1bda043ac4493f0391b6db274aa083
Kieran Desoer <kieran.desoer1@engineering.digital.dwp.gov.uk>
2024-06-07 15:51:43 +0100

    Initial commit for stage 11, includes reducer

--- src/components/Person.tsx
+++ src/components/Person.tsx
@@ -1,25 +1,36 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'updatePersonName':
+            return { ...state,  updatePersonName: action.payload };
+        case 'updatePersonAge':
+            return { ...state, updatePersonAge: action.payload };
+        case 'tgEdit':
+            return { ...state, editing: !state.editing };
+        default:
+            return
+    }
+}
 
 export const Person = ({person, deletePerson, editPerson}) => {
-    const [updatePersonName, setUpdatePersonName] = useState(person.name);
-    const [updatePersonAge, setUpdatePersonAge] = useState(person.age);
-    const [editing, setEditing] = useState(false);
+    const [state, dispatch] = useReducer( reducer, { updatePersonName: person.name, updatePersonAge: person.age, editing: false })
 
-    const editOnClick = (originalPerson, newPerson) => {
-        if (editing) {
-            editPerson(originalPerson, newPerson);
+    const editOnClick = (originalPerson, updatedPerson) => {
+        if (state.editing) {
+            editPerson(originalPerson, updatedPerson);
         }
-        setEditing(!editing);
+        dispatch({ type: 'tgEdit' });
+        // setEditing(!editing);
     }
 
     return <div style={{ padding: 10 }} key={`${person.name}, ${person.age}`}>
-        <div>Name: {person.name} Age: {person.age}</div>
         <div>
             <label htmlFor="editPersonName">Name: </label>
-            <input disabled={!editing} type='text' placeholder="Enter New Name" id='editPersonName' value={updatePersonName} onChange={(e) => setUpdatePersonName(e.target.value)} />
+            <input disabled={!state.editing} type='text' placeholder="Enter New Name" id='editPersonName' value={state.updatePersonName} onChange={(e) => dispatch({ type: 'updatePersonName', payload: e.target.value })} />
             <label htmlFor="editPersonAge">Age: </label>
-            <input disabled={!editing} type='number' placeholder="Entner New Age" id='editPersonAge' value={updatePersonAge} onChange={(e) => setUpdatePersonAge(Number(e.target.value))} />
-            <button style={{ marginLeft: 10 }} onClick={() => editOnClick({name: person.name, age: person.age}, {name: updatePersonName, age: updatePersonAge})}>{editing ? `Confirm` : `Edit`}</button>
+            <input disabled={!state.editing} type='number' placeholder="Entner New Age" id='editPersonAge' value={state.updatePersonAge} onChange={(e) => dispatch({ type: 'updatePersonAge', payload: e.target.value })} />
+            <button style={{ marginLeft: 10 }} onClick={() => editOnClick({name: person.name, age: person.age}, {name: state.updatePersonName, age: state.updatePersonAge})}>{state.editing ? `Confirm` : `Edit`}</button>
             <button style={{ marginLeft: 10 }} onClick={() => deletePerson({ name: person.name, age: person.age })}>Delete</button>
         </div>
     </div>
